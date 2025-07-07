39611fbda127da3a9b56ca9d79d94576089a2733
Sandratra Rabenanahary <sandratrahasinarivelo5@gmail.com>
2025-07-07 23:28:36 +0300

    modification ajax.js

--- ajax.js
+++ ajax.js
@@ -1,4 +1,4 @@
-const apiBase = "http://localhost:80/ETU003197/t/Examen-Projet-Final-S4-2025/ws";
+const apiBase = "http://localhost:80/ETU003197/t/Examen-Projet-Finale-S4-2025/ws";
 
 function ajax(method, url, data, callback) {
     const xhr = new XMLHttpRequest();
