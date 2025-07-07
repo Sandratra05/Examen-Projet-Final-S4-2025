67e49bc307c945948d2342002a47d6e2cd78dae8
Sandratra Rabenanahary <sandratrahasinarivelo5@gmail.com>
2025-07-07 16:25:00 +0300

    config pour les renders

--- ws/index.php
+++ ws/index.php
@@ -2,5 +2,9 @@
 require 'vendor/autoload.php';
 require 'db.php';
 require 'routes/etudiant_routes.php';
+// require 'routes/fond_routes.php';
+require_once __DIR__ . '/routes/fond_routes.php';
+
+Flight::set('flight.views.path', __DIR__ . '/../');
 
 Flight::start();
